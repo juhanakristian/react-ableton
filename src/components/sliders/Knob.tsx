357c92aba1ed3ebf79b15775b90ee1e248590ca4
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2023-07-27 23:38:23 +0300

    wip: knob drag to change value

--- src/components/sliders/Knob.tsx
+++ src/components/sliders/Knob.tsx
@@ -3,17 +3,80 @@ import styled from "@emotion/styled";
 import React from "react";
 import { arcPath } from "./util";
 
-interface KnobProps extends React.InputHTMLAttributes<HTMLInputElement> {
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const KnobTitle = styled.label`
+  font-family: Sans-Serif;
+  font-size: 0.8em;
+  color: #000000;
+`;
+
+const KnobValue = styled.span`
+  font-family: Sans-Serif;
+  font-size: 0.9em;
+  color: #000000;
+`;
+
+type KnobProps = {
   value: number;
   disabled?: boolean;
-}
+  title: string | React.ReactNode;
+  onChange?: (value: number) => void;
+} & React.InputHTMLAttributes<HTMLInputElement>;
 
 function Knob(props: KnobProps, ref: React.ForwardedRef<HTMLInputElement>) {
+  const [dragStart, setDragStart] = React.useState<[number, number]>([0, 0]);
+
   const arc = arcPath(25, 25, 20, 130, 410);
   const valueArc = arcPath(25, 25, 20, 130, 130 + (410 - 130) * props.value);
+
+  function onMouseMove(event: MouseEvent) {
+    const value = Math.min(
+      1,
+      Math.max(0, props.value + (dragStart[1] - event.clientY) / 100)
+    );
+
+    if (props.onChange) {
+      props.onChange(value);
+    }
+    console.log(value);
+  }
+
+  function onMouseUp(event: MouseEvent) {
+    document.removeEventListener("mousemove", onMouseMove);
+    document.removeEventListener("mouseup", onMouseUp);
+  }
+
+  function onMouseDown(event: React.MouseEvent<any>) {
+    event.preventDefault();
+
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+
+    const startX = event.clientX;
+    const startY = event.clientY;
+
+    setDragStart([startX, startY]);
+  }
+
   return (
-    <>
-      <svg width={60} height={60}>
+    <Container>
+      {typeof props.title === "string" ? (
+        <KnobTitle>{props.title}</KnobTitle>
+      ) : (
+        props.title
+      )}
+      <svg
+        width={50}
+        height={45}
+        style={{ border: "1px solid red" }}
+        onMouseDown={onMouseDown}
+      >
         <path
           d={arc}
           fill="none"
@@ -29,10 +92,18 @@ function Knob(props: KnobProps, ref: React.ForwardedRef<HTMLInputElement>) {
           stroke="#54CFE8"
         />
       </svg>
-      <input hidden ref={ref} {...props} value={props.value} type="range">
+      <KnobValue>{props.value}</KnobValue>
+      <input
+        hidden
+        ref={ref}
+        {...props}
+        value={props.value}
+        type="range"
+        onChange={props.onChange}
+      >
         {props.children}
       </input>
-    </>
+    </Container>
   );
 }
 
