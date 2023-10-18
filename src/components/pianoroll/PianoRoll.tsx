63585a51de56370c3065454669adce19bb9d3ac7
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2023-10-18 23:29:43 +0300

    wip: pianoroll keys

--- src/components/pianoroll/PianoRoll.tsx
+++ src/components/pianoroll/PianoRoll.tsx
@@ -1,13 +1,78 @@
-import styled from "@emotion/styled";
 import React from "react";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  display: flex;
+`;
 
 const ScrollContainer = styled.div`
   overflow-x: scroll;
   overflow-y: scroll;
   height: 100%;
   width: 100%;
+  max-height: 300px;
 `;
+
+// top with prop
+const BlackKey = styled.div`
+  background-color: black;
+  border: 1px solid black;
+  height: 20px;
+  width: 50px;
+`;
+
+const WhiteKey = styled.div`
+  background-color: white;
+  border: 1px solid black;
+  border-top: none;
+  height: 20px;
+  width: 50px;
+  :first-of-type {
+    border-top: 1px solid black;
+  }
+`;
+
+const OctaveContainer = styled.div`
+  position: relative;
+`;
+
+const KeysContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+function Octave() {
+  return (
+    <OctaveContainer>
+      <KeysContainer>
+        <WhiteKey />
+        <BlackKey />
+        <WhiteKey />
+        <BlackKey />
+        <WhiteKey />
+        <BlackKey />
+        <WhiteKey />
+        <WhiteKey />
+        <BlackKey />
+        <WhiteKey />
+        <BlackKey />
+        <WhiteKey />
+      </KeysContainer>
+    </OctaveContainer>
+  );
+}
 
 export type PianoRollProps = {};
 
-export default function PianoRoll({}: PianoRollProps) {}
+export default function PianoRoll({}: PianoRollProps) {
+  return (
+    <Container>
+      <ScrollContainer>
+        <Octave />
+        <Octave />
+        <Octave />
+        <Octave />
+      </ScrollContainer>
+    </Container>
+  );
+}
---
+++ stories/PianoRoll.stories.ts
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import PianoRoll from "../src/components/pianoroll/PianoRoll";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: "Example/PianoRoll",
+  component: PianoRoll,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+} satisfies Meta<typeof PianoRoll>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Dark: Story = {
+  args: { children: "Rounded Button" },
+};
+
+export const Light: Story = {
+  args: { children: "Rounded Button", variant: "light" },
+};
