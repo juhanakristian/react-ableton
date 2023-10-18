a88e2c3edcc9dc1cbe1f3ababc04256fa583c764
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2023-10-18 23:05:00 +0300

    wip: PianoRoll

---
+++ src/components/pianoroll/PianoRoll.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+const ScrollContainer = styled.div`
+  overflow-x: scroll;
+  overflow-y: scroll;
+  height: 100%;
+  width: 100%;
+`;
+
+export type PianoRollProps = {};
+
+export default function PianoRoll({}: PianoRollProps) {}
